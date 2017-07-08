f5fdea1fdb58d7a816a0ba2977658562d3a87214
Délita Makanda <delita.makanda@gmail.com>
2017-07-08 11:48:55 +0200

    feature toggle details

--- src/Card.js
+++ src/Card.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import CheckList from './CheckList';
 
 class Card extends Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+            showDetails: false
+        };
+    }
 
     render() {
+        let cardDetails;
+        if (this.state.showDetails) {
+            cardDetails = (
+                <div className="card__details">{this.props.description}
+                <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+                </div>
+            );
+        };
+
         return (
             <div className="card">
-                <div className="card__title">{this.props.title}</div>
-                <div className="card__details">{this.props.description}</div>
-                <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+                <div className="card__title" onClick={()=>this.setState({showDetails: !this.state.showDetails})}>{this.props.title}</div>
+                {cardDetails}
             </div>
         );
     }
