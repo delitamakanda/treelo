b604b666fc6996592cc2d69e4737cf7b8ad39d0e
Délita Makanda <delita.makanda@gmail.com>
2017-07-08 11:51:37 +0200

    refactor toggleDetails

--- src/Card.js
+++ src/Card.js
@@ -9,6 +9,10 @@ class Card extends Component {
         };
     }
 
+    toggleDetails() {
+        this.setState({showDetails: !this.state.showDetails});
+    }
+
     render() {
         let cardDetails;
         if (this.state.showDetails) {
@@ -21,7 +25,7 @@ class Card extends Component {
 
         return (
             <div className="card">
-                <div className="card__title" onClick={()=>this.setState({showDetails: !this.state.showDetails})}>{this.props.title}</div>
+                <div className="card__title" onClick={this.toggleDetails.bind(this)}>{this.props.title}</div>
                 {cardDetails}
             </div>
         );
