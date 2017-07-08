a22f2f54161439251cb925d7e821cd02918573ce
Délita Makanda <delita.makanda@gmail.com>
2017-07-08 15:54:30 +0200

    refactor treelo add tasks

--- src/TrelloBoardContainer.js
+++ src/TrelloBoardContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TrelloBoard from './TrelloBoard';
 import 'whatwg-fetch';
+import update from 'react-addons-update';
 import 'babel-polyfill';
 
 const API_URL = 'http://kanbanapi.pro-react.com';
@@ -18,10 +19,14 @@ class TrelloBoardContainer extends Component {
     }
 
     componentDidMount() {
-        fetch(API_URL + '/cards', {headers: API_HEADERS})
+        fetch(`${API_URL}/cards`, {headers: API_HEADERS})
         .then((response) => response.json())
         .then((responseData) => {
-            this.setState({cards: responseData});
+            this.setState({
+                cards: responseData
+            });
+
+            window.state = this.state;
         })
         .catch((error) => {
             console.log('error fecthing data', error);
@@ -30,13 +35,101 @@ class TrelloBoardContainer extends Component {
 
     addTask(cardId, taskName) {
 
+        let prevState = this.state;
+
+        let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
+
+        let newTask = {id: Date.now(), name: taskName, done: false};
+
+        let nextState = update(this.state.cards, {
+            [cardIndex]: {
+                tasks: {$push: [newTask]}
+            }
+        });
+
+        this.setState({cards: nextState});
+
+        fetch(`${API_URL}/cards/${cardId}/tasks`, {
+            method: 'post',
+            headers: API_HEADERS,
+            body: JSON.stringify(newTask)
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error("Server response not ok");
+            }
+        })
+        .then((responseData) => {
+            newTask.id = responseData.id
+            this.setState({cards: nextState});
+        })
+        .catch((error) => {
+            this.setState(prevState);
+        });
     }
 
     deleteTask(cardId, taskId, taskIndex) {
+        let prevState = this.state;
 
+        let cardIndex = this.state.cards.findIndex((card)=>card.id == cardId);
+
+        let nextState = update(this.state.cards, {
+            [cardIndex]: {
+                tasks: {$splice: [[taskIndex, 1]]}
+            }
+        });
+        this.setState({cards: nextState})
+        fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+            method: 'delete',
+            headers: API_HEADERS
+        })
+        .then((response) => {
+            if(!response.ok) {
+                throw new Error("server not ok")
+            }
+        })
+        .catch((error) => {
+            console.error("fetch error", error)
+            this.setState(prevState);
+        })
     }
 
     toggleTask(cardId, taskId, taskIndex) {
+        let prevState = this.state;
+        let cardIndex = this.state.cards.findIndex((card) => card.id == cardId);
+        let newDoneValue;
+
+        let nextState = update(this.state.cards, {
+            [cardIndex]: {
+                tasks: {
+                    [taskIndex]: {
+                        done: { $apply: (done) => {
+                            newDoneValue = !done
+                            return newDoneValue;
+                        }}
+                    }
+                }
+            }
+        });
+
+        this.setState({cards: nextState});
+
+        fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+            method: 'put',
+            headers: API_HEADERS,
+            body: JSON.stringify({done:newDoneValue})
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("server not ok", error);
+            }
+        })
+        .catch((error) => {
+            console.error("fetch error", error)
+            this.setState(prevState);
+        });
 
     }
 
