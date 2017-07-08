08086c23f7b04bf7cb0ad36ecfca5c7867ab919f
Délita Makanda <delita.makanda@gmail.com>
2017-07-08 12:22:41 +0200

    add task form

--- src/CheckList.js
+++ src/CheckList.js
@@ -16,6 +16,7 @@ class CheckList extends Component {
                 <ul>
                     {tasks}
                 </ul>
+                <input type="text" className="checklist--add-task" placeholder="Type then hit Enter to add a task" />
             </div>
         );
     }
