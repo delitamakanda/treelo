07fb520c35bbab87694fedf487163cbf1ec732bb
Délita Makanda <delita.makanda@gmail.com>
2018-07-28 21:26:15 +0200

    fix bugs

--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -11,7 +11,7 @@ class CheckList extends Component {
 
     render() {
         let tasks = this.props.tasks.map((task, taskIndex) => (
-            <li key={task.id} className="checklist__task">
+            <li key={taskIndex} className="checklist__task">
                 <input type="checkbox" defaultChecked={task.done} onChange={this.props.taskCallbacks.toggle.bind(null, this.props.cardId, task.id, taskIndex)} />
                 {task.name} {' '}
                 <a href="#" className="checklist__task--remove" onClick={this.props.taskCallbacks.delete.bind(null, this.props.cardId, task.id, taskIndex)}></a>
