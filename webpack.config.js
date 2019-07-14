f436b71ff0f815bcbf63e6fac92b23cd05293b71
Délita Makanda <delita.makanda@gmail.com>
2019-07-14 17:58:39 +0200

    build task

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-module.exports = {
-    entry: [
-        './src/App.js'
-    ],
+var webpack = require('webpack');
+
+var config = {
+    devtool: 'eval-source-map',
+    entry: __dirname + '/src/App.js',
     output: {
-        path: __dirname,
+        path: __dirname + '/public',
         filename: 'bundle.js'
     },
     module: {
@@ -15,5 +16,24 @@ module.exports = {
                 presets: ['es2015', 'react']
             }
         }]
+    },
+    devServer: {
+        contentBase: './public',
+        colors: true,
+        historyApiFallback: true,
+        inline: true
     }
 };
+
+if (process.env.NODE_ENV === 'production') {
+    config.devtool = false;
+    config.plugins = [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({comments: false}),
+        new webpack.DefinePlugin({
+            'process.env' : {NODE_ENV: JSON.stringify('production')}
+        })
+    ];
+};
+
+module.exports = config;
